f1a6f59ea590196f2125faf188f4bb6303b3f4b4
profflaviomoreni <profflavio.moreni@fiap.com.br>
2023-01-24 23:24:49 -0300

    parsing finish

--- Fiap.Api.WebClient/Program.cs
+++ Fiap.Api.WebClient/Program.cs
@@ -1,5 +1,6 @@
-﻿
-using System.Text;
+﻿using System.Text;
+using Fiap.Api.WebClient.Models;
+using Newtonsoft.Json;
 
 namespace Fiap.Api.WebClient;
 class Program
@@ -13,15 +14,57 @@ class Program
         Console.Read();
     }
 
+    static void get()
+    {
+        // Criando um objeto Cliente para conectar com o recurso.
+        HttpClient client = new HttpClient();
+
+        // Execute o método Get passando a url da API e salvando o resultado.
+        // em um objeto do tipo HttpResponseMessage
+        HttpResponseMessage resposta = client.GetAsync(urlEndPoint).Result;
+
+        // Verifica se o Status Code é 200.
+        if (resposta.IsSuccessStatusCode)
+        {
+            // Recupera o conteúdo JSON retornado pela API
+            string conteudo = resposta.Content.ReadAsStringAsync().Result;
+
+            // Convertendo o conteúdo em uma lista de Cliente
+            List<ClienteModel> lista =
+                JsonConvert.DeserializeObject<List<ClienteModel>>(conteudo);
+
+            // Imprime o conteúdo na janela Console
+            foreach (var item in lista)
+            {
+                Console.WriteLine("Nome:" + item.Nome);
+                Console.WriteLine("Nascimento:" + item.DataNascimento);
+                Console.WriteLine(" ========== ");
+                Console.WriteLine("");
+            }
+
+        }
+        else
+        {
+            Console.WriteLine("Erro:" + resposta.StatusCode + " - " + resposta.Content);
+        }
+
+    }
+
     static void post()
     {
         // Criando um objeto Cliente para conectar com o recurso.
         HttpClient client = new HttpClient();
 
-        // Conteúdo do tipo de produto em JSON.
-        string json = "{ \"nome\": \"Cauani Sanches\" , \"dataNascimento\": \"1990-03-01T00:00:00\" , \"observacao\": \"Texto de Observação\" , \"representanteId\": 1}";
+        ClienteModel clienteModel = new ClienteModel();
+        clienteModel.Nome = "Fátima Silva";
+        clienteModel.RepresentanteId = 2;
+        clienteModel.DataNascimento = new DateTime();
+        clienteModel.Observacao = "Obs Obs";
 
-        // Convertendo texto para JSON StringContent. 
+        // Conteudo do Cliente em JSON
+        var json = JsonConvert.SerializeObject(clienteModel);
+
+        // Convertendo texto para JSON StringContent 
         StringContent conteudo = new StringContent(json.ToString(), Encoding.UTF8, "application/json");
 
         // Execute o método POST passando a url da API 
@@ -40,24 +83,5 @@ class Program
     }
 
 
-    static void get()
-    {
-        // Criando um objeto Cliente para conectar com o recurso.
-        HttpClient client = new HttpClient();
-
-        // Execute o método Get passando a url da API e salvando o resultado.
-        // em um objeto do tipo HttpResponseMessage
-        HttpResponseMessage resposta = client.GetAsync(urlEndPoint).Result;
-
-        // Verifica se o Status Code é 200.
-        if (resposta.IsSuccessStatusCode)
-        {
-            // Recupera o conteúdo JSON retornado pela API
-            string conteudo = resposta.Content.ReadAsStringAsync().Result;
-
-            // Imprime o conteúdo na janela Console.
-            Console.Write(conteudo.ToString());
-        }
-
-    }
+    
 }
